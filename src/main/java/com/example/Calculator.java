6c0b66cd054bbff983fd0fbe893b848d6562526c
Deepak Katariya <deepak.sorthiya@gmail.com>
2020-09-10 09:58:04 +0530

    Update Calculator.java

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -3,6 +3,10 @@ package com.example;
 public class Calculator {
 
 	public int addTwoNumbers(int a, int b) {
+		System.out.println("sgsgsgsggsgsd1");
+		System.out.println("sgsgsgsggsgsd1");
+		System.out.println("sgsgsgsggsgsd1");
+		System.out.println("sgsgsgsggsgsd1");
 		return a + b;
 	}
 
