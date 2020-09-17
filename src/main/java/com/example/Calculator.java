11f49f54df890b643bf3b2113c2314baf199d46e
Deepak Katariya <deepak.sorthiya@gmail.com>
2020-09-17 17:06:29 +0530

    omo

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,13 +1,26 @@
 package com.example;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class Calculator implements Calc {
 
 	@Override
 	public int addTwoNumbers(int a, int b) {
+		Properties properties = new Properties();
+		try {
+			properties.load(getClass().getResourceAsStream("/config/env.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		a++;
 		b++;
 		System.out.println("gdhgdhgfdh");
 		return a + b;
+	}
+
+	public static void main(String[] args) {
+		new Calculator().addTwoNumbers(1, 2);
 	}
 
 }
